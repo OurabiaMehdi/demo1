7f2d3881b4adc13eb4ce5bd57afc9caf6623b97d
Taha <tahamiinee@gmail.com>
2026-03-12 14:24:24 +0100

    Change the name of Analysis's course from 4_5823376840445664012.pdf -->TaylorExpansion.pdf

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const app = require(path.resolve(__dirname, 'src/app.js'))
-//const config = require(path.resolve(__dirname,'./src/config/config.js'))
+const config = require(path.resolve(__dirname,'./src/config/config.js'))
 
 
 app.listen(config.port , ()=>{
